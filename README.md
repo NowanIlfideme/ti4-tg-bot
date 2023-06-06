9328debaec64780d30197c7e33786d0f2b8847a2
NowanIlfideme <git@nowan.dev>
2023-06-06 13:48:43 +0200

    aiogram3

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ti4-tg-bot
+
 Telegram bot for TI4
--- src/ti4_tg_bot/main.py
+++ src/ti4_tg_bot/main.py
@@ -1,25 +1,63 @@
 """Main loop."""
 
+import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, executor
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.filters import Command
 from aiogram.types import Message
 
-logger = logging.getLogger(__name__)
-
 with open("secret/tg_token") as f:
-    bot = Bot(f.read(), parse_mode="HTML")
-dp = Dispatcher(bot=bot)
+    TOKEN = f.read()
+
+# All handlers should be attached to the Router (or Dispatcher)
+router = Router()
 
 
-@dp.message_handler()
-async def echo(message: Message):
-    """Echo bot."""
-    await message.answer(message.text)
+@router.message(Command(commands=["start"]))
+async def command_start_handler(message: Message) -> None:
+    """
+    This handler receive messages with `/start` command
+    """
+    # Most event objects have aliases for API methods that can be called in events' context
+    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
+    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
+    # method automatically or call API method directly via
+    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
+    await message.answer(f"Hello, <b>{message.from_user.full_name}!</b>")
+
+
+@router.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward received message back to the sender
+
+    By default, message handler will handle all message types (like text, photo, sticker and etc.)
+    """
+    try:
+        # Send copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def async_main() -> None:
+    # Dispatcher is a root router
+    dp = Dispatcher()
+    # ... and all other routers should be attached to Dispatcher
+    dp.include_router(router)
+
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(TOKEN, parse_mode="HTML")
+    # And the run events dispatching
+    await dp.start_polling(bot)
 
 
 def main():
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
