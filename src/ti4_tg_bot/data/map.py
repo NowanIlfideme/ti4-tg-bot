b9402f6564b3dd063dae4331579eeb2af6c66690
NowanIlfideme <git@nowan.dev>
2023-09-27 09:48:49 +0200

    Initial part towards local map gen.

---
+++ src/ti4_tg_bot/data/map.py
@@ -0,0 +1,38 @@
+"""Data models for the map."""
+
+from .models import *  # FIXME specific imports
+
+
+Idx = tuple[int, int]
+Adj = tuple[Idx, Idx]
+
+
+class GalaxyShape(BaseModel):
+    """The shape of a galaxy."""
+
+    valid_positions: list[Idx]
+    home_positions: dict[int, list[Idx]]  # per player count
+    hyper_lanes: list[Adj] = []
+
+
+class GalaxyLayout(GalaxyShape):
+    """A pre-set layout."""
+
+    layout_name: str
+
+
+class UnsetTile(Tile):
+    """Unset placeholder tile."""
+
+
+class PlayerTile(UnsetTile):
+    """Tile assigned to a player, but otherwise unset."""
+
+    player: str
+
+
+class GalaxyTiles(BaseModel):
+    """Define a galaxy with tiles.
+
+    State might be empty, in-progress or completed.
+    """
---
+++ src/ti4_tg_bot/mapgen/local.py
@@ -0,0 +1,1 @@
+"""Generate map locally."""
